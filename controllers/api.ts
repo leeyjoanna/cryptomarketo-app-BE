d587ed138ee17cdfb32208e8e2cdc1672dd25595
Joanna <leejoanna94@gmail.com>
2021-12-03 22:50:21 -0800

    added API calls for news, organized API return data to types

--- controllers/api.ts
+++ controllers/api.ts
@@ -1,41 +1,16 @@
-// api calls to mongodb to get existing lists
+// missing api calls to mongodb to get existing lists
 import express, { Response } from 'express'
 import axios from 'axios'
 import config from '../utils/config'
 const API_KEY = config.API_KEY
 import MarketoList from '../models/marketoList'
-import { PolygonData, CoinName } from '../my-module'
+import { PolygonData, CoinName, PolygonCoinData, CoinInfo, PolygonCoinNews, CoinNews } from '../my-module'
 import { URLSearchParams } from 'url'
 // import Coin from '../models/coin'
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    return res.json('blank url')
-})
-
-// router.get('/search', async (req, res) => {
-//     console.log('query', req.query[0])
-//     const searchTerm = req.query[0];
-//     const url = `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${searchTerm}&apikey=${API_KEY}`;
-//     try{
-//         const vantageResult = await axios.get(url, {
-//             headers:{'User-Agent': 'request'
-//             }
-//         });
-//         console.log('result', vantageResult.data.bestMatches)
-//         // iterate through result, cut out and organize information into cleaner objects (typed for FE)
-//         // then push those into array and send that over to FE to use
-//         return res.json(vantageResult.data.bestMatches)
-//     } catch (err) {
-//         console.log(err);
-//         res.status(400).json({
-//             body: 'oops, something went wrong with the search!'
-//         })
-//     }    
-// })
-
- // changing to polygon API, use search param for search drop down, click only, on click run individual ticker API to load coin page
+// search API to Polygon.io for list of coins
 router.get('/search', async (req, res) => {
   console.log('query', req.query[0])
   const searchTerm = req.query[0];
@@ -59,33 +34,65 @@ router.get('/search', async (req, res) => {
         body: 'oops, something went wrong with the search!'
     })
 }    
-  // axios
-  //   .get( url, {
-  //     headers: { Authorization: `Bearer ${API_KEY}` 
-  //     }
-  //   })
-  //   .then(result => {
-  //     // console.log('search result', result)
-  //     console.log(result.data.results)
-  //     let listNames:polygonName = []
-  //     result.data.results.forEach(({ name }) => listNames.push(name))
-  //     // res.json(result.data.businesses)
-  //   })
-  //   .catch(e => console.log(e));
-
 })
 
-router.get('/:url', (req, res) => {
-    const url = req.params.url
-    console.log('backend', url)
 
-    if (url===""){
-        return res.json('empty')
+// search API to Polygon.io for specific coin information
+router.get('/coin/:coinID', async (req, res) => {
+  const coinTicker = req.params.coinID;
+  const date = (new Date()).toISOString().split('T')[0]
+  console.log('date', date)
+  const url = `https://api.polygon.io/v1/open-close/crypto/${coinTicker}/USD/${date}?adjusted=true`;
+  try{
+    const polygonReturn = await axios.get(url, {
+        headers: { Authorization: `Bearer ${API_KEY}` 
+        }
+      });
+    const polygonResults:PolygonCoinData = polygonReturn.data;
+    
+    console.log('api results', polygonResults);
+    let coinInfo:CoinInfo = {
+      symbol: polygonResults.symbol,
+      day: polygonResults.day,
+      open: polygonResults.open,
+      close: polygonResults.close
     }
-    return res.json('stuff')
+    console.log('new coin obj', coinInfo);
+    return res.json(coinInfo)
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({
+        body: 'oops, something went wrong with the search!'
+    })
+}    
 })
 
+// search API to Polygon.io for specific coin information
+router.get('/coinNews/:coinID', async (req, res) => {
+  const coinTicker = req.params.coinID;
 
+  const url = `https://api.polygon.io/v2/reference/news?ticker=${coinTicker}&limit=5`;
+  try{
+    const polygonReturn = await axios.get(url, {
+        headers: { Authorization: `Bearer ${API_KEY}` 
+        }
+      });
+    const polygonResults:PolygonCoinNews[] = polygonReturn.data.results;
+    
+    console.log('api results', polygonResults);
+    let coinNews:CoinNews[] = []
+    polygonResults?.forEach(({ title, author, article_url, description, keywords }) => coinNews.push({title, author, article_url, description, keywords}))
+    console.log('coin news obj', coinNews);
+    return res.json(coinNews)
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({
+        body: 'oops, something went wrong with the search!'
+    })
+}    
+})
+
+// api call to MongoDB to get list
 router.get('/myList/:uuid', async (req, res) => {
     const url = req.params.uuid
 
--- my-module.d.ts
+++ my-module.d.ts
@@ -17,3 +17,56 @@ export interface CoinName{
     base_currency_symbol: string;
     base_currency_name: string;
 }
+
+export interface PolygonCoinData{
+    symbol: string,
+    isUTC: boolean,
+    day: string,
+    open: number,
+    close: number,
+    openTrades: Trades[],
+}
+
+export interface Trades{
+    x: number,
+    p: number,
+    s: number,
+    c: number[],
+    i: string,
+    t: number
+}
+
+// coin info custom obj 
+export interface CoinInfo{
+    symbol: string,
+    day: string,
+    open: number,
+    close: number
+}
+
+export interface PolygonCoinNews{
+    id: string,
+    publisher: {
+        name: string,
+        homepage_url: string,
+        logo_url: string,
+        favicon_url: string
+    },
+    title: string,
+    author: string,
+    published_utc: string,
+    article_url: string,
+    tickers: string[],
+    amp_url: string,
+    image_url: string,
+    description: string,
+    keywords: string[]
+}
+
+export interface CoinNews{
+    title: string,
+    author: string,
+    article_url: string,
+    description: string,
+    keywords: string[]
+}
